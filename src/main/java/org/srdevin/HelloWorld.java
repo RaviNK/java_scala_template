b8910bcd0abd4ba929e7f506db97e4262b28bbb4
RaviNK <nkravikumar90@gmail.com>
2017-03-18 21:02:20 +0530

    clean up

--- src/main/java/org/srdevin/HelloWorld.java
+++ src/main/java/org/srdevin/HelloWorld.java
@@ -6,7 +6,7 @@ package org.srdevin;
  */
 public class HelloWorld {
 
-    public static void main (String ... args){
+    public static void main (String[] args){
 
         System.out.println("Hello , Get ready !!! ");
     }
